65e870a44a987f2e2ee6b343c7fdff18b89f5df1
guka20 <gukadavitadze23@gmail.com>
2023-09-07 19:59:05 +0400

    secret key

--- middleware/requestMiddleware.js
+++ middleware/requestMiddleware.js
@@ -1,5 +1,8 @@
 const requestMiddleware = (req, res, next) => {
   const { secretkey } = req.headers;
+  if (!secretkey) {
+    return res.json({ message: "To access api please provide secret key" });
+  }
   if (secretkey !== process.env.SECRETACCESSKEY) {
     return res.json({ message: "Unsecured request, provide secret key" });
   }
